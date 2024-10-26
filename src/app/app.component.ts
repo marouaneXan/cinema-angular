3ce631dad3301c13efb2d43fd0b8e9a179e07284
marouane zahaoui <marouanezahaoui835@gmail.com>
2024-10-26 19:30:45 +0100

    add some components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CitiesComponent } from './components/cities/cities.component';
+import { CinemasComponent } from './components/cinemas/cinemas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbNavModule,NavbarComponent,CitiesComponent,CinemasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/cinemas/cinemas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinemas',
+  standalone: true,
+  imports: [],
+  templateUrl: './cinemas.component.html',
+  styleUrl: './cinemas.component.scss'
+})
+export class CinemasComponent {
+
+}
